a9604bd4d28892883a5560a08118f11abbf736ec
baopham2537 <baro3275@gmail.com>
2026-01-11 23:19:39 +0700

    xong 7,8

--- Migrations/20260111160154_Rating.Designer.cs
+++ Migrations/20260111160154_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TuNhanTamTinh.Data;
 
@@ -11,9 +12,11 @@ using TuNhanTamTinh.Data;
 namespace TuNhanTamTinh.Migrations
 {
     [DbContext(typeof(TuNhanTamTinhContext))]
-    partial class TuNhanTamTinhContextModelSnapshot : ModelSnapshot
+    [Migration("20260111160154_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Food.cs
+++ Models/Food.cs
@@ -7,17 +7,33 @@ namespace TuNhanTamTinh.Models
     public class Food
     {
         public int Id { get; set; }
+
         [DisplayName("Name")]
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? FoodName { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Manufacturer { get; set; }
 
         [Display(Name = "Release Date")]
+
         [DataType(DataType.Date), DisplayName("MFD")]
         public DateTime ManufacturingDate { get; set; }
 
         [DataType(DataType.Date), DisplayName("EXP")]
         public DateTime ExpiryDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
     }
 }
